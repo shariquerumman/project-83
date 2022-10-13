c35d7a2a2ba1ac441c6755b5420c75333b80d00b
shariquerumman <86907710+shariquerumman@users.noreply.github.com>
2022-10-13 09:16:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+
+var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    width_of_line = 2;
+
+    var width = screen.width;
+
+var new_width = screen.width-70;
+var new_height = screen.height-300;
+ if(width < 992 ){
+
+    canvas.width=new_width;
+    canvas.height=new_height;
+    document.body.style.overflow="hidden";
+    
+ }
+
+    canvas.addEventListener("touchstart", my_touchStart);
+    
+    function my_touchStart(e)
+    {
+
+
+        last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
+        last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
+    }
+
+
+    
+
+    canvas.addEventListener("touchmove", my_touchMove);
+    function my_touchMove(e)
+    {
+
+         current_position_of_mouse_x = e.touches[0].clientX - canvas.offsetLeft;
+         current_position_of_mouse_y = e.touches[0].clientY - canvas.offsetTop;
+
+        if (mouseEvent="mouseDown") {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+
+        console.log("Last position of x and y coordinates = ");
+        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+        ctx.moveTo(last_position_of_x, last_position_of_y);
+ 
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        ctx.stroke();
+        }
+
+        last_position_of_x = current_position_of_mouse_x; 
+        last_position_of_y = current_position_of_mouse_y;
+
+
+    }
+
